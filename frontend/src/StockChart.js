7769bdd3d8d2506a5949a7c805f360a6e85da955
Julian Loiacono <jcloiacon@gmail.com>
2023-06-12 15:48:41 -0400

    data graphing is up

--- frontend/src/StockChart.js
+++ frontend/src/StockChart.js
@@ -6,13 +6,17 @@ import { Chart, CategoryScale, LinearScale, PointElement, LineElement, Title, To
 Chart.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip);
 
 const StockChart = ({ stockPrices }) => {
+// Get the symbol from the first price in stockPrices
+  const symbol = stockPrices.length > 0 ? stockPrices[0].symbol : '';
+
   // Convert the stockPrices data to chart.js format
   const chartData = {
-    labels: stockPrices.map((stock) => stock.date),
+    
+    labels: stockPrices.map((stock) => stock.begins_at),
     datasets: [
       {
         label: 'Stock Price',
-        data: stockPrices.map((stock) => stock.bid_price),
+        data: stockPrices.map((stock) => stock.close_price),
         fill: false,
         borderColor: 'rgb(75, 192, 192)',
         tension: 0.1,
@@ -22,7 +26,7 @@ const StockChart = ({ stockPrices }) => {
 
   return (
     <div>
-      <h2>Stock Chart</h2>
+      <h2>{symbol}</h2>
       <Line data={chartData} />
     </div>
   );
